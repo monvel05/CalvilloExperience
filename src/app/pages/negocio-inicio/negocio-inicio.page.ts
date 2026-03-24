258acb018516806348b32833303a421319789831
KaritoEscalera <clauditaescalera@gmail.com>
2026-03-24 10:06:37 -0600

    feat: interfaces de turista-inicio y negocio-inicio con navegción

--- src/app/pages/negocio-inicio/negocio-inicio.page.ts
+++ src/app/pages/negocio-inicio/negocio-inicio.page.ts
@@ -1,20 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { DatabaseService } from '../turista-inicio/services/database.service';
 
 @Component({
   selector: 'app-negocio-inicio',
   templateUrl: './negocio-inicio.page.html',
   styleUrls: ['./negocio-inicio.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule]
 })
 export class NegocioInicioPage implements OnInit {
+  
+  datosNegocio: any = {
+    nombre: '',
+    visitas: 0,
+    contactos: 0,
+    rating: 0,
+    resenas: 0,
+    promociones: 0
+  };
+  idUsuario: number = 1;
 
-  constructor() { }
+  constructor(
+    private db: DatabaseService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.cargarDatos();
   }
 
+  cargarDatos() {
+    this.db.getPerfilNegocio(this.idUsuario).subscribe({
+      next: (res: any) => {
+        this.datosNegocio = res;
+        console.log('¡Lo logramos¡:', res);
+      },
+      error: (err: any) => {
+        console.error('Error al cargar datos:', err);
+      }
+    });
+  }
+
+  editarInformacion() {
+    this.router.navigate(['/negocio-iniciao/editar']);
+  }
+
+  verGaleria() {
+    this.router.navigate(['/negocio-iniciao/galeria']);
+  }
+
+  verResenas() {
+    this.router.navigate(['/negocio-iniciao/resenas']);
+  }
+
+  verPromociones() {
+    this.router.navigate(['/negocio-iniciao/promociones']);
+  }
+
+  agregarProducto() {
+    this.router.navigate(['/negocio-iniciao/agregar-producto']);
+  }
+
+  cerrarSesion() {
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/pages/turista-inicio/turista-inicio.page.ts
+++ src/app/pages/turista-inicio/turista-inicio.page.ts
@@ -1,20 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { 
+  IonContent, IonHeader, IonTitle, IonToolbar, 
+  IonButton, IonIcon, IonCard, IonCardContent, 
+  IonGrid, IonRow, IonCol, IonBadge 
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-turista-inicio',
   templateUrl: './turista-inicio.page.html',
   styleUrls: ['./turista-inicio.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    IonButton, 
+    IonIcon, 
+    IonCard, 
+    IonCardContent, 
+    IonGrid, 
+    IonRow, 
+    IonCol,
+    IonBadge
+  ]
 })
 export class TuristaInicioPage implements OnInit {
-
-  constructor() { }
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  irAlMapa() {
+    this.router.navigate(['/mapa']);
+  }
+
+  irACenadurias() {
+    this.router.navigate(['/cenadurias']);
+  }
+
+  irAComidaRapida() {
+    this.router.navigate(['/comida-rapida']);
+  }
+
+  irAHospedaje() {
+    this.router.navigate(['/hospedaje']);
+  }
+
+  irAAtractivos() {
+    this.router.navigate(['/atractivos']);
+  }
+
+  irAlLogin() {
+    this.router.navigate(['/login']);
+  }
 }
